847dc1e881d1c5b4ccdcf5b52163f50cf3587713
Kamily vitoria <kamily0012@gmail.com>
2025-08-21 11:12:09 -0300

    Update variaveis.py

--- variaveis.py
+++ variaveis.py
@@ -1,2 +1,5 @@
 nome_cliente= "Kamily vitoria" #string
-print ("nome", nome_cliente")
+print ("nome", nome_cliente)
+# Variavel booleana
+situaçao_cliente = True
+print("Qual é a situação do cliente?", situaçao_cliente)
